24ca67e92794c01f3d7d725fee9549fe5a1c3fa4
Shahil Zhangada <shahilgreenz@yahoo.com>
2018-10-27 16:16:55 -0700

    Minor update to parsing Indeed job descriptions

--- parse/IndeedParser.py
+++ parse/IndeedParser.py
@@ -1,33 +1,42 @@
 from entities.JobInfo import JobInfo
 from parse.BaseParser import BaseParser
 
+from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 
 class IndeedParser(BaseParser):
     """ Represents a parser for Indeed. """
     BASE_URL = "https://www.indeed.com/"
+    JOB_SECTION_HEADER = "jobSectionHeader"
 
-
-    def _init__(self, html_page):
+    def __init__(self, html_page):
         """ Initializes the data."""
-        super()._init__(html_page)
+        super().__init__(html_page)
 
     def extract_job_results(self):
-        test_description = 0
+        desc_number = 0
         # print(self.html_page)
         # Iterate through all job results for Indeed (on the first page).
         for job_row in self.html_page.find_all(name="div", attrs={"data-tn-component": "organicJob"}):
             # Grab basic info for jobInfo based on the results page.
-            self.setup_job_info(job_row)
-            with open("samples/indeed/descriptions/description" + str(test_description) + ".html", "r") as job_description:
+            job_info = self.setup_job_info(job_row)
+            with open("samples/indeed/descriptions/description" + str(desc_number) + ".html", "r") as job_description:
                 description_sample = job_description.read()
-                self.filter_job_description(description_sample)
+                description_soup = BeautifulSoup(description_sample, "html.parser")
+                should_include_job_info = self.filter_job_description(description_soup)
                 job_description.close()
+                if should_include_job_info:
+                    self.job_results.append(job_info)
 
         print(len(self.job_results))
 
     def setup_job_info(self, job_row):
+        """
+        Returns and sets up the intial jobInfo acquired from the results page.
+        :param job_row: a row from the job results page.
+        :return: jobInfo
+        """
         # Parse company name.
         company = job_row.find(name="span", attrs={"class", "company"}).text.strip()
         # Parse listed location from result.
@@ -41,12 +50,15 @@ class IndeedParser(BaseParser):
         salary_element = snippet.find(name="span", attrs={"class", "no-wrap"})
         salary = salary_element.text.strip() if salary_element else "N/A"
         job_info = JobInfo(title, location, url, company, salary)
-        self.job_results.append(job_info)
         print(job_info)
+        return job_info
 
     def filter_job_description(self, job_description):
         """
         Helper method to parse values and perform filtering for a job description.
         :return: boolean to indicate whether we should filter the result or not.
         """
-        print(job_description)
+        criteria = job_description.find_all(name="h3", attrs={"class", self.JOB_SECTION_HEADER})
+        if criteria:
+            print([criterion.parent for criterion in criteria])
+        return True
