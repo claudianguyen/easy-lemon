2308ded9f4346db5bcb43db8847048a5738dfe46
Shahil Zhangada <shahilgreenz@yahoo.com>
2019-02-19 02:20:27 -0800

    Can now handle multiple requests; created pipeline for computing job_points

--- easy-lemon.py
+++ easy-lemon.py
@@ -71,87 +71,19 @@ def execute_indeed_query(job_query):
         '-a',
         'url=' + indeed_url_creator.generate_url(),
         '-a',
-        'num_results=' + '50',
-        '-o',
-        'output.json'
+        'num_results=' + '50'
     ])
 
     # Return results back to frontend.
-    with open("output.json") as items_file:
-        job_results = json.load(items_file)
+    job_results = []
+    with open("output.json", "r+") as items_file:
+        for job_result in items_file:
+            job_results.append(json.loads(job_result))
         return json.dumps(
             sorted(
-                [job_result for job_result in job_results],
-                key=lambda result: compute_job_result_priority(result, job_query),
+                job_results,
+                key=lambda result: result["job_points"],
                 reverse=True))
-
-
-def compute_job_result_priority(job_result, job_query):
-    """
-    Computes a "point-value" for the given job_result, which will be used for prioritization of the job_results.
-    :param job_result: The job_result to prioritize.
-    :param job_query: The job_query that the user provided.
-    :return: Point value for this job_result.
-    """
-    points = 0
-    points += compute_salary_points(job_result['job_salary'], job_query) \
-              + compute_exp_points(job_result['job_exp'], job_query)
-    return points
-
-
-def compute_exp_points(exp, job_query):
-    """
-    Computes the "point-value" for the given number of years of experience.
-    :param exp: Raw string representing the number of years of experience.
-    :param job_query: Job_query provided by the user.
-    :return: Point value for the given number of years of experience.
-    """
-    desired_exp = 2
-    if exp == "N/A":
-        return 0
-    # Years of experience might be a range, so parse out the hyphen.
-    years_of_exp = exp.split('-')
-    # May contain a '+', so replace that with whitespace.
-    exp_low = years_of_exp[0].replace('+', '').strip()
-    if len(years_of_exp) > 1:
-        exp_high = years_of_exp[1].replace('+', '').strip()
-    exp_number = int(exp_low)
-    # points = 0.2 - abs(exp_number - desired_exp) / 50
-    points = 1 - abs(exp_number - desired_exp) / 50
-    return points
-
-
-def compute_salary_points(salary, job_query):
-    """
-    Computes the "point-value" for the job_salary portion.
-    1: greater than expected
-    0.5: No salary information
-    0: Below expected.
-    :param salary: The salary portion saved by the job_result.
-    :param job_query: The job_query provided by the user.
-    :return:
-    """
-    if salary == "N/A" or job_query.get_job_salary() is None:
-        return 0.5
-    try:
-        desired_salary = int(FormatUtils.currency_string_to_basic_string(job_query.get_job_salary()))
-        # For now, we bank on the low side of the salary.
-        salary_range = salary.split('-')
-        if len(salary_range) > 1:
-            salary_range[0] = FormatUtils.currency_string_to_basic_string(salary_range[0])
-            # Second number represents range, may have additional characters after it. We should remove these.
-            salary_range[1].split()
-            salary_number_high = int(FormatUtils.currency_string_to_basic_string(salary_range[1][0].strip()))
-            salary_number_low = int(salary_range[0])
-            return 1 \
-                if (salary_number_low and salary_number_low > desired_salary or salary_number_high > desired_salary) \
-                else 0
-
-        else:
-            salary_number = int(salary_range[0].replace(',', '').strip())
-            return 1 if salary_number and salary_number > desired_salary else 0
-    except ValueError:
-        return 0.5
 
 
 def build_job_query(job_title, job_location, job_salary=""):
--- settings.py
+++ settings.py
@@ -21,6 +21,10 @@ NEWSPIDER_MODULE = 'spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+# Feed export settings.
+FEED_FORMAT = "jsonlines"
+FEED_URI = "output.json"
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
@@ -64,9 +68,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'pipelines.EasyLemonPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'pipelines.EasyLemonPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
